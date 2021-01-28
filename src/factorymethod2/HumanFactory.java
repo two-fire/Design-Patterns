9b19fdafc76d49d279414e533fd8e16ec3bc111f
two-fire <1538002378@qq.com>
2021-01-28 21:27:30 +0800

    工厂方法模式

--- src/factorymethod2/HumanFactory.java
+++ src/factorymethod2/HumanFactory.java
@@ -1,14 +1,27 @@
 package factorymethod2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * 工厂模式还有一个非常重要的应用，就是延迟化。
+ * 这个在类初始化很消耗资源的情况比较实用，通过这种方式可以很好的减少项目的复杂程度
+ * 在代码中增加MAP即可。
+ */
 public class HumanFactory {
+    //定义一个MAP,初始化过的Human对象都放在这里
+    private static HashMap<String, Human> humans = new HashMap<>();
     //定一个烤箱，泥巴塞进去，人就出来
     public static Human createHuman(Class c) {
         Human human = null;
         try {
-            human = (Human) Class.forName(c.getName()).newInstance(); //产生一个人类
+            //如果MAP中有，则直接从取出，不用初始化了
+            if (humans.containsKey(c.getSimpleName())) {
+                human = humans.get(c.getSimpleName());
+            } else
+                human = (Human) Class.forName(c.getName()).newInstance(); //产生一个人类
+                humans.put(c.getSimpleName(), human);
         } catch (InstantiationException e) {//你要是不说个人类颜色的话，没法烤，要白的黑，你说话了才好烤
             System.out.println("必须指定人类的颜色");
         } catch (IllegalAccessException e) {//定义的人类有问题，那就烤不出来了，这是..
